0b035d1fb80cb77af381881e0311cc27c6758797
Paradox696969 <80007621+Paradox696969@users.noreply.github.com>
2022-10-10 21:59:56 +0100

    Ahmad Complete Deletion of excess columns

--- Separator.py
+++ Separator.py
@@ -3,6 +3,27 @@ import pandas
 pandas.set_option('display.max_columns', None)
 df = pandas.read_csv("Crime_data.csv")
 
-df.drop(["DR_NO", "AREA", "Rpt Dist No"], axis=1, inplace=True)
+df.drop([
+        "DR_NO",
+        "AREA",
+        "Rpt Dist No", 
+        "Part 1-2", 
+        "Crm Cd", 
+        "Mocodes", 
+        "Premis Cd" , 
+        "Weapon Used Cd" , 
+        "Status" , 
+        "Status Desc" , 
+        "Crm Cd 1",
+        "Crm Cd 2",
+        "Crm Cd 3",
+        "Crm Cd 4",
+        "LOCATION",
+        "Cross Street",
+        "LAT",
+        "LON"
+        ], 
+        axis=1, inplace=True)
+
 
 print(df)
