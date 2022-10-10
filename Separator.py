21fe4183a8305c2555886951517f3bbf21b50562
Paradox696969 <80007621+Paradox696969@users.noreply.github.com>
2022-10-10 14:55:07 +0100

    Update Separator.py

--- Separator.py
+++ Separator.py
@@ -1,1 +1,8 @@
+import pandas
 
+pandas.set_option('display.max_columns', None)
+df = pandas.read_csv("Crime_data.csv")
+
+df.drop(["DR_NO", "AREA", "Rpt Dist No"], axis=1, inplace=True)
+
+print(df)
